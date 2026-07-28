54ac917a2d10ed3f70e8806b8740a2a36482286c
belatakabe19-droid <belatakabe19@gmail.com>
2026-07-28 13:12:42 +0700

    Create app.py
    echo "# bostone-digital-app" >> README.md
    git init
    git add README.md
    git commit -m "first commit"
    git branch -M main
    git remote add origin https://github.com/belatakabe19-droid/bostone-digital-app.git
    git push -u origin main

---
+++ app.py
@@ -0,0 +1,7 @@
+echo "# bostone-digital-app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/belatakabe19-droid/bostone-digital-app.git
+git push -u origin main
